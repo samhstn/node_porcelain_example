0042268a9de7f73b50b904ff582261812537e6ac
shouston3 <shouston3@me.com>
2017-12-24 17:14:01 +0000

    kill node before and after request

--- lib/node_porcelain_example.ex
+++ lib/node_porcelain_example.ex
@@ -6,11 +6,27 @@ defmodule NodePorcelainExample do
 
   @root_url "http://localhost:4444"
 
+  defp kill_node do
+    {ps_procs, 0} = System.cmd "ps", ["-ef"]
+
+    node_proc_pid = ps_procs
+    |> String.split("\n")
+    |> Enum.find(&(Regex.match? ~r/\d node \S*node_app\/server\/index.js/, &1))
+    |> case do
+      nil -> nil
+      n_pid -> n_pid |> String.split |> Enum.at(1)
+    end
+
+    if node_proc_pid do
+      System.cmd "kill", ["-9", node_proc_pid]
+    end
+  end
+
   @doc"""
   See https://stackoverflow.com/questions/39772835/how-to-shutdown-an-external-process-that-was-started/39772976#39772976 for perhaps a better way of doing this.
   """
   def request do
-    System.cmd "pkill", ["node"]
+    kill_node()
 
     cmd = "node node_app/server/index.js"
     opts = [out: {:send, self()}]
@@ -22,7 +38,7 @@ defmodule NodePorcelainExample do
         HTTPoison.get! @root_url <> "/"
     end
 
-    {"", 0} = System.cmd "pkill", ["node"]
+    kill_node()
 
     with %HTTPoison.Response{body: body} <- result, do: body
   end
